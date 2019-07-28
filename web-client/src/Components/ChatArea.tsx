abcdbf87a4a2e437142674d71227231b13bab5b8
Matthew Caloger <mattcaloger@gmail.com>
2019-07-28 00:20:16 -0400

    Overflow handling and appearance update

--- web-client/src/Components/ChatArea.tsx
+++ web-client/src/Components/ChatArea.tsx
@@ -1,57 +1,61 @@
-import React, { useState, useEffect, useRef } from 'react'
-import styled from 'styled-components';
-import ChatItem from './ChatItem';
-import { socket } from '../api/socket'
-import { Message } from '../Models/Message';
+import React, { useState, useEffect, useRef } from "react";
+import styled from "styled-components";
+import ChatItem from "./ChatItem";
+import { socket } from "../api/socket";
+import { Message } from "../Models/Message";
 
 const ChatContainer = styled.div`
-    margin: 10px;
-    flex: 1;
-    background: #fff;
-    border-radius: 10px;
-    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
-    transition: all 0.3s cubic-bezier(.25,.8,.25,1);
-    
-    
-`
+  margin: 10px;
+  flex: 1;
+  background: #fff;
+  border-radius: 10px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+`;
 
 const ChatAreaContainer = styled.div`
-    flex: 1;
-    background: #eee;
-    z-index: 1;
-    overflow-y: scroll;
-    
-`
+  flex: 1;
+  background: #eee;
+  z-index: 1;
+  overflow-x: none;
+  overflow-y: auto;
+`;
 
 export default function ChatArea() {
-    const initialState: Message[] = [];
-    const [messages, setMessages] = useState(initialState);
-    const scrollDummy = useRef<HTMLDivElement | null>(null);
+  const initialState: Message[] = [];
+  const [messages, setMessages] = useState(initialState);
+  const scrollDummy = useRef<HTMLDivElement | null>(null);
 
-    const scrollToBottom = () => {
-        scrollDummy.current.scrollIntoView({ behavior: "smooth" })
-      }
+  const scrollToBottom = () => {
+    scrollDummy.current.scrollIntoView({ behavior: "smooth" });
+  };
 
-    useEffect(() => {
-        // setup subscription
-        socket.on('receiveMessage', (message: Message) => {
-            
-            setMessages([...messages, message])
-            document.title = `${messages.length+1} Messages`
-            scrollToBottom()
-        });
-        // unsubscribe on unmount
-        return () => {
-            socket.removeEventListener('receiveMessage')
-        }
-    }, [messages])
-    
-    return (
-        <ChatAreaContainer>
-            <ChatContainer>
-                {messages.map((message: Message, index: number) => <ChatItem key={index} username={message.user} message={message.message} timestamp={message.timestamp}></ChatItem>)}
-                <div ref={scrollDummy}></div>
-            </ChatContainer>
-        </ChatAreaContainer>
-    )
+  useEffect(() => {
+    // setup subscription
+    socket.on("receiveMessage", (message: Message) => {
+      setMessages([...messages, message]);
+      document.title = `${messages.length + 1} Messages`;
+      scrollToBottom();
+    });
+    // unsubscribe on unmount
+    return () => {
+      socket.removeEventListener("receiveMessage");
+    };
+  }, [messages]);
+
+  return (
+    <ChatAreaContainer>
+      <ChatContainer>
+        {messages.map((message: Message, index: number) => (
+          <ChatItem
+            key={index}
+            username={message.user}
+            message={message.message}
+            timestamp={message.timestamp}
+          />
+        ))}
+        <div ref={scrollDummy} />
+      </ChatContainer>
+    </ChatAreaContainer>
+  );
 }
--- web-client/src/Components/ChatItem.tsx
+++ web-client/src/Components/ChatItem.tsx
@@ -1,42 +1,43 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 export default function ChatItem(props: any) {
-    const { username, timestamp, message } = props
+  const { username, timestamp, message } = props;
 
-    const MessageContainer = styled.div`
-        display: flex; 
-        padding: 10px;
-        background: #fff;
-        border-radius: 5px;
-        &:nth-child(2n) {
-            background: #eee;
-        }   
-    `
+  const MessageContainer = styled.div`
+    display: flex;
+    padding: 10px;
+    background: #fff;
+    border-radius: 5px;
+    overflow-x: none;
+    overflow-wrap: break-word;
+    &:nth-child(2n) {
+      background: #eee;
+    }
+  `;
 
-    const UsernameContainer = styled.div`
-        font-weight: bold;
-        margin-right: 5px;
-        
-    `
+  const UsernameContainer = styled.div`
+    font-weight: bold;
+    margin-right: 5px;
+  `;
 
-    const MessageText = styled.div`
-        word-wrap: break-word;
-        white-space: initial;
-        flex: 1;
-    `
+  const MessageText = styled.div`
+    word-wrap: break-word;
+    flex: 1;
+    margin-left: 5px;
+  `;
 
-    const TimestampContainer = styled.div`
-        font-style: italic;
-        color: grey;
-        margin-right: 5px;
-    `
+  const TimestampContainer = styled.div`
+    font-style: italic;
+    color: grey;
+    margin-right: 5px;
+  `;
 
-    return (
-        <MessageContainer>
-            <UsernameContainer>{username}</UsernameContainer>
-            <TimestampContainer>{timestamp}</TimestampContainer>
-            <MessageText>{message}</MessageText>
-        </MessageContainer>
-    )
+  return (
+    <MessageContainer>
+      <UsernameContainer>{username}</UsernameContainer>
+      <TimestampContainer>{timestamp}</TimestampContainer>
+      <MessageText>{message}</MessageText>
+    </MessageContainer>
+  );
 }
